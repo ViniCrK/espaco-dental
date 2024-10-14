6217c3a07dd0c4b16a2463c99dc7d09e5c3a78b3
ViniCrK <vinicrk@outlook.com>
2024-10-14 18:59:56 -0300

    Create footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,60 @@
+export function Footer() {
+  return (
+    <>
+      <footer className="py-8 px-24 text-white bg-slate-950">
+        <div className="flex justify-between">
+          <div className="flex gap-3 mb-4">
+            <div className="bg-white rounded h-10 w-10">
+              <figure>
+                <img src="assets/espaco-dental-logo.jpg" alt="Logo" />
+              </figure>
+            </div>
+            <div className="bg-white rounded h-10 w-10">
+              <figure>
+                <img src="" alt="" />
+              </figure>
+            </div>
+            <div className="bg-white rounded h-10 w-10">
+              <figure>
+                <img src="" alt="" />
+              </figure>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="bg-yellow-300 h-1 w-12 mb-4"></div>
+            <ul className="space-y-2">
+              <li>
+                <a href="#" className="hover:text-yellow-300">
+                  Item 1
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="bg-yellow-300 h-1 w-12 mb-4"></div>
+            <ul className="space-y-2">
+              <li>
+                <a href="#" className="hover:text-yellow-300">
+                  Item 1
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="bg-yellow-300 h-1 w-12 mb-4"></div>
+            <ul className="space-y-2">
+              <li>
+                <a href="#" className="hover:text-yellow-300">
+                  Item 1
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
