a7dba1bc15d1d5cb7a1cd985169408d372ede7b3
ViniCrK <vinicrk@outlook.com>
2024-10-14 19:00:29 -0300

    Create users interfaces

---
+++ src/models/Users.ts
@@ -0,0 +1,17 @@
+export interface IPatient {
+  name: string;
+  email: string;
+  cpf: string;
+  birth_date: string;
+  gender: string;
+  phone: string;
+}
+
+export interface IDentist {
+  name: string;
+  email: string;
+  cpf: string;
+  cro: string;
+  specialty: string;
+  phone: string;
+}
