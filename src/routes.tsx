5ea300ca48d231ed7c0a419b30afd75d83aeb93d
ViniCrK <vinicrk@outlook.com>
2024-10-14 18:59:45 -0300

    Set login route

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { BaseLayout } from "./pages/_layouts/base";
 import { ListAppointments } from "./pages/app/list-appointments";
 import { ListPatients } from "./pages/app/list-patients";
 import { HomePage } from "./pages/app/homepage";
+import { Login } from "./pages/app/login";
 
 export const router = createBrowserRouter([
   {
@@ -14,13 +15,17 @@ export const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
+        path: "login",
+        element: <Login />,
+      },
+      {
         path: "consultas",
         element: <ListAppointments />,
       },
       {
         path: "pacientes",
         element: <ListPatients />,
-      }
-    ]
-  }
-])
+      },
+    ],
+  },
+]);
