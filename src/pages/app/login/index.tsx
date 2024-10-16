397a8ee4318a0fc2851bb96f05063bfaf3f832f9
ViniCrK <vinicrk@outlook.com>
2024-10-16 17:03:54 -0300

    Remove logic of this component

--- src/pages/app/login/index.tsx
+++ src/pages/app/login/index.tsx
@@ -1,73 +1,9 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { LoginForm } from "./components/login-form";
 
 export function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post("http://127.0.0.1:8000/api/token/", {
-        username,
-        password,
-      });
-
-      const { access } = response.data;
-      localStorage.setItem("token", access);
-
-      navigate("/");
-
-      return access;
-    } catch (error) {
-      alert(`Login mal-sucedido: ${error}`);
-    }
-  };
-
   return (
     <>
-      <div className="max-w-md mx-auto p-8 rounded-lg bg-slate-950">
-        <h1 className="text-2xl text-white font-bold mb-4">Login</h1>
-        <form onSubmit={handleLogin} className="space-y-4">
-          <div>
-            <label htmlFor="username" className="text-white block font-medium">
-              Usuário:
-            </label>
-            <input
-              id="username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded"
-              required
-            />
-          </div>
-
-          <div>
-            <label htmlFor="password" className="text-white block font-medium">
-              Senha:
-            </label>
-            <input
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded"
-              required
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition"
-          >
-            Entrar
-          </button>
-        </form>
-      </div>
+      <LoginForm />
     </>
   );
 }
