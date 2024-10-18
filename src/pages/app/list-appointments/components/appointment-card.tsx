cd4909f278e068b3040b1807d0e1b96d326192e1
ViniCrK <vinicrk@outlook.com>
2024-10-18 16:25:08 -0300

    Update component

--- src/pages/app/list-appointments/components/appointment-card.tsx
+++ src/pages/app/list-appointments/components/appointment-card.tsx
@@ -1,15 +1,49 @@
-import { IDentist, IPatient } from "../../../../models/Users";
+import { IAppointment } from "../../../../models/Appointments";
 
 interface AppointmentCardProps {
-  patient: IPatient;
-  dentist: IDentist;
+  appointment: IAppointment;
 }
 
-export function AppointmentCard({ patient, dentist }: AppointmentCardProps) {
+export function AppointmentCard({ appointment }: AppointmentCardProps) {
   return (
     <>
-      <h1>Nome do Paciente: {patient.name}</h1>
-      <h1>Nome do Dentista: {dentist.name}</h1>
+      <div className="max-w-sm bg-white rounded-lg shadow-md dark:bg-zinc-800">
+        <img
+          className="rounded-t-lg w-full h-48 object-cover"
+          src="src\assets\espaco-dental-logo.jpg"
+          alt="Imagem do paciente"
+        />
+        <div className="px-5 py-5">
+          <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {appointment.patient.name}
+          </h5>
+
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Dentista: {appointment.dentist.name}
+          </p>
+
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Procedimento(s):{" "}
+            {appointment.procedures_done.map((procedure) => procedure.name)}
+          </p>
+
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Status: {appointment.status.toUpperCase()}
+          </p>
+
+          <div className="flex justify-between items-end">
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Marcada para: {appointment.date}
+            </p>
+          </div>
+
+          <div className="flex justify-end mt-4">
+            <button className="bg-sky-900 text-white hover:bg-sky-700 font-semibold py-2 px-4 rounded">
+              Ver Detalhes
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
