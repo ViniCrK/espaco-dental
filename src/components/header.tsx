3ffe656bba6ed4450da159f491297756be64f27a
ViniCrK <vinicrk@outlook.com>
2024-10-18 16:23:55 -0300

    Fix style and Add Link to site logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,33 +6,39 @@ export function Header() {
       <header className="flex h-[90px] px-24 bg-slate-950">
         <div className="flex items-center justify-between w-full">
           <figure>
-            <img className="h-[60px] w-[60px] rounded-lg" src="" alt="Logo" />
+            <Link to="">
+              <img
+                className="h-[60px] w-[60px] rounded-lg"
+                src="src\assets\espaco-dental-logo.jpg"
+                alt="Logo"
+              />
+            </Link>
           </figure>
 
           <nav className="flex gap-4">
             <Link
-              className="text-white text-lg font-normal hover:text-yellow-300"
+              className="text-white text-lg font-normal hover:text-yellow-400"
               to=""
             >
               Início
             </Link>
 
             <Link
-              className="text-white text-lg font-normal hover:text-yellow-300"
+              className="text-white text-lg font-normal hover:text-yellow-400"
               to="consultas"
             >
               Consultas
             </Link>
 
             <Link
-              className="text-white text-lg font-normal hover:text-yellow-300"
+              className="text-white text-lg font-normal hover:text-yellow-400"
               to="pacientes"
             >
               Pacientes
             </Link>
 
             <Link
-              className="text-white text-lg font-normal pl-10 hover:text-yellow-300"
+              className="text-white text-lg font-normal pl-10 hover:text-yellow-400"
               to="login"
             >
               Login
